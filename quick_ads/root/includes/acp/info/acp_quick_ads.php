9847643b293de404722d504b13dfcf82493ab9b1
Huynh Buu Tam <o0johntam0o@gmail.com>
2013-11-15 13:27:24 +0700

    Release version 1.1.3
    Nothing changed in this version

--- quick_ads/root/includes/acp/info/acp_quick_ads.php
+++ quick_ads/root/includes/acp/info/acp_quick_ads.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Quick Ads
-* @version 1.1.2 of 06.11.2013
+* @version 1.1.3 of 12.11.2013
 * @copyright (c) 2012 o0johntam0o - o0johntam0o@gmail.com
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
@@ -26,7 +26,7 @@ class acp_quick_ads_info
 		return array(
 			'filename'	=> 'acp_quick_ads',
 			'title'		=> 'QUICK_ADS_TITLE_ACP',
-			'version'	=> '1.1.2',
+			'version'	=> '1.1.3',
 			'modes'		=> array(
 				'index'	=> array(
 					'title'			=> 'QUICK_ADS_TITLE1',
